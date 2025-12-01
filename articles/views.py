8ecf3382010f3fdc45413366c21de837605a7027
iitsfaded <kyle.kalway@snhu.edu>
2025-12-01 11:20:13 -0500

    permissions and authorizations

--- articles/views.py
+++ articles/views.py
@@ -1,35 +1,33 @@
 # articles/views.py
-from django.views.generic import ListView, DetailView # new
-from django.views.generic.edit import UpdateView, DeleteView, CreateView # new
-from django.urls import reverse_lazy # new
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin # new
+)
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.urls import reverse_lazy
+
 from .models import Article
 
-class ArticleCreateView(CreateView): # new
-    model = Article
-    template_name = "article_new.html"
-    fields = (
-        "title",
-        "body",
-        "author",
-    )
+class ArticleUpdateView(
+    LoginRequiredMixin, UserPassesTestMixin, UpdateView): # new
+        model = Article
+        fields = (
+            "title",
+            "body",
+        )
+        template_name = "article_edit.html"
+        
+        def test_func(self): # new
+            obj = self.get_object()
+            return obj.author == self.request.user
 
-class ArticleListView(ListView):
-    model = Article
-    template_name = "article_list.html"
-
-class ArticleDetailView(DetailView): # new
-    model = Article
-    template_name = "article_detail.html"
-
-class ArticleUpdateView(UpdateView): # new
-    model = Article
-    fields = (
-        "title",
-        "body",
-    )
-    template_name = "article_edit.html"
-
-class ArticleDeleteView(DeleteView): # new
-    model = Article
-    template_name = "article_delete.html"
-    success_url = reverse_lazy("article_list")
+class ArticleDeleteView(
+    LoginRequiredMixin, UserPassesTestMixin, DeleteView): # new
+        model = Article
+        template_name = "article_delete.html"
+        success_url = reverse_lazy("article_list")
+        
+        def test_func(self): # new
+            obj = self.get_object()
+            return obj.author == self.request.user
